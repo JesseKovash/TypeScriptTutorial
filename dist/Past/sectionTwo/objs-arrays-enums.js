4276680239d3c2a5ed39f0b19f021966205c4855
Jesse Kovash <jessekovash@gmail.com>
2022-05-19 14:21:22 -0600

    changed file structure to modular architecture. Also used ES module import statements in necessary places. retained full functionality.

---
+++ dist/Past/sectionTwo/objs-arrays-enums.js
@@ -0,0 +1,8 @@
+"use strict";
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
+;
